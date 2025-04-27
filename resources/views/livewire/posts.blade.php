1e59328581e190a2eafae6a4021b9264253fed1c
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-04-27 09:54:43 +0530

    created a static liveware component which has table with static post data

---
+++ resources/views/livewire/posts.blade.php
@@ -0,0 +1,20 @@
+<div>
+<table class="min-w-full divide-y divide-gray-300">
+    <thead>
+        <tr class="hover:bg-gray-200">
+            <th scope="col" class="px-6 py-3 text-start text-sm uppercase">ID</th>
+            <th scope="col" class="px-6 py-3 text-start text-sm uppercase">User</th>
+            <th scope="col" class="px-6 py-3 text-start text-sm uppercase ">Title</th>
+            <th scope="col" class="px-6 py-3 text-start text-sm uppercase">Created</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="hover:bg-gray-200">
+            <td class="px-6 py-3 text-start text-sm ">1</td>
+            <td class="px-6 py-3 text-start text-sm ">Muzaffar Shaikh</td>
+            <td class="px-6 py-3 text-start text-sm ">Laravel Blog</td>
+            <td class="px-6 py-3 text-start text-sm ">just now</td>
+        </tr>
+    </tbody>
+</table>
+</div>
